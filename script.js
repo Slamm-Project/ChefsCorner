9bae107c29d4d4612789bffa1985a338d5eaea06
ShivsTKD <shivanmaharaj.sm@gmail.com>
2021-03-23 16:21:35 +0000

    added transitions and fixed the image loading issue

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 let index = -1;
-let images = ['h1.jpeg','h2.jpeg','h3.jpeg','h4.jpeg','h5.jpeg','h6.jpeg','h7.jpeg',];
+let images = ['images/h1.jpeg','images/h2.jpeg','images/h3.jpeg','images/h4.jpeg','images/h5.jpeg'];
 showSlides();
-
 function showSlides(){
   index++;
   if (index + 1 > images.length){
     index = 0;
   }
+  
   document.getElementById("HomeBG").style.backgroundImage = "url("+images[index]+")";
   document.getElementById("HomeBG").style.backgroundSize = "cover";
-  document.getElementById("HomeBG").style.backgroundPosition = "top";
-  
-  setTimeout(showSlides, 5000);
+  document.getElementById("HomeBG").style.backgroundPosition = "center top";
+  document.getElementById("HomeBG").classList.add("animate");
+   
+  setTimeout(function(){
+    document.getElementById("HomeBG").classList.remove("animate");
+    document.getElementById("HomeBG").offsetWidth;
+    showSlides();
+  }, 5000);
 }
